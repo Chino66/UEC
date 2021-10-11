8410f29bdcf078cc0a569c28db5f69c80ea8e081
Chino66 <447660276@qq.com>
2021-10-12 00:23:11 +0800

    0

---
+++ Assets/_package_/Editor/UPMConfig.cs
@@ -0,0 +1,22 @@
+using System;
+using System.IO;
+
+namespace UEC
+{
+    public class UPMConfig
+    {
+        private const string UPMConfigFile = ".upmconfig.toml";
+
+        public static string UPMConfigPath => Path.Combine(UserProfilePath(), UPMConfigFile);
+
+        public static string UserProfilePath()
+        {
+            return Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+        }
+
+        public static string SpecialFolder(Environment.SpecialFolder folder)
+        {
+            return Environment.GetFolderPath(folder);
+        }
+    }
+}
--- Assets/_package_/Editor/UPMExtension/UPMExtensionUI.cs
+++ Assets/_package_/Editor/UPMExtension/UPMExtensionUI.cs
@@ -1,6 +1,11 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
 using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.UIElements;
+using Button = UnityEngine.UIElements.Button;
 
 namespace UEC
 {
@@ -15,20 +20,44 @@ namespace UEC
 
         private UPMExtensionUI()
         {
-            // _root = new VisualElement {name = "root"};
-            // Add(_root);
-            //
-            // var lab = new Label();
-            // lab.text = "sssssssssssssssssssss";
-            // _root.Add(lab);
-            //
-            //
-            // var btn = new Button();
-            // btn.text = "ssss";
-            // _root.Add(btn);
-            InitPackageVersion();
+            _root = new VisualElement {name = "root"};
+            Add(_root);
+
+            var box = new Box();
+            box.style.flexDirection = new StyleEnum<FlexDirection>(FlexDirection.Row);
+            _root.Add(box);
+            {
+                var textField = new TextField("Username");
+                textField.value = "Enter Github Username";
+                box.Add(textField);
+
+                textField = new TextField("Token");
+                textField.value = "Enter Github Token";
+                box.Add(textField);
+            }
+
+
+            var btn = new Button();
+            btn.text = "show path";
+            _root.Add(btn);
+            btn.clickable.clicked += () =>
+            {
+                var lines = File.ReadAllLines(UPMConfig.UPMConfigPath);
+
+                foreach (var line in lines)
+                {
+                    Debug.Log(line);
+                }
+            };
         }
-        
+
+        private void ShowPath(Environment.SpecialFolder folder)
+        {
+            var path = UPMConfig.SpecialFolder(folder);
+
+            Debug.Log($"Environment.SpecialFolder is {folder}, path is {path}");
+        }
+
         private VisualElement _packageVersionRoot;
 
         private List<string> _tagsList;
@@ -39,7 +68,7 @@ namespace UEC
         private VisualTreeAsset _dependenciesItemVisualTreeAsset;
 
         private Queue<VisualElement> _dependenciesItemQueue;
-        
+
         private void InitPackageVersion()
         {
             _packageVersionRoot = new VisualElement {name = "package_version_root"};
