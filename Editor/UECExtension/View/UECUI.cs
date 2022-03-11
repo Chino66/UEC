37bb7ffb428530a96172a8f4fd79a76348d1810d
cyj <447660276@qq.com>
2022-03-11 18:06:42 +0800

    context

--- Editor/UECExtension/View/UECUI.cs
+++ Editor/UECExtension/View/UECUI.cs
@@ -6,7 +6,7 @@ using UPMEnvironmentConfigure;
 
 namespace UEC
 {
-    public class UECUI : UI
+    public class UECUI : UI<UECContext>
     {
         public static UECUI CreateUI(VisualElement parent = null)
         {
@@ -20,15 +20,17 @@ namespace UEC
             return ui;
         }
 
-        public UECConfigModel UecConfig { get; }
-        private UPMConfigModel _upmConfig;
-        private ManifestModel _manifest;
+        // public UECConfigModel UecConfig { get; }
+        // private UPMConfigModel _upmConfig;
+        // private ManifestModel _manifest;
 
         private UECUI()
         {
-            UecConfig = new UECConfigModel();
-            _upmConfig = new UPMConfigModel();
-            _manifest = new ManifestModel();
+            // UecConfig = new UECConfigModel();
+            // _upmConfig = new UPMConfigModel();
+            // _manifest = new ManifestModel();
+            
+            UIFramework.Context
         }
 
         protected override void OnInitialize(VisualElement parent)
